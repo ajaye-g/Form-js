c19843cd7be6ff8265742cc9bcb47a74b397f806
ram <ramzzprakash.j.d2029@gmail.com>
2022-09-09 17:03:19 +0530

    newcommit

--- js/script1.js
+++
@@ -1,9 +0,0 @@
-function validateform(){  
-var fname=document.myform.fname.value; 
-var lname=document.myform.lname.value;
-var password=document.myform.password.value;  
-  
-if (fname==null || fname==""){  
-  alert("First name can't be blank");  
-  return false; 
-}
